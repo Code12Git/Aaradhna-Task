098170613c67f9c34709fdda0561b84bbb58041d
saxenasaksham46 <saxenasaksham46>
2025-07-14 15:43:27 +0530

    Added Configuration for vercel

--- server/src/server.js
+++ server/src/server.js
@@ -8,13 +8,35 @@ const cors = require('cors');
 const app = express();
 const PORT = fromEnv('PORT') || 3001;
 
+const allowedOrigins = [
+  'https://aaradhna-task.vercel.app/',
+  /\.vercel\.app$/, 
+  /\.now\.sh$/,    
+  'http://localhost:5173', 
+  
+].filter(Boolean);
 
-
-app.use(cors({
-  origin: 'http://localhost:5173',
+const corsOptions = {
+  origin: (origin, callback) => {
+     if (!origin) return callback(null, true);
+    
+     if (allowedOrigins.some(pattern => {
+      if (typeof pattern === 'string') {
+        return origin === pattern;
+      } else if (pattern instanceof RegExp) {
+        return pattern.test(origin);
+      }
+      return false;
+    })) {
+      return callback(null, true);
+    }
+    
+    callback(new Error('Not allowed by CORS'));
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-}));
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/api', routes);
